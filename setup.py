a78d136dca3b948e596a53e2785ceba29dcaf9bc
nicewang <wangxiaonannice@gmail.com>
2026-03-18 20:36:39 +0800

    cmt-20260318-5
    modify version no.: v0.1.0 -> v0.1.1
    
    Signed-off-by: nicewang <wangxiaonannice@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mpc_python",
-    version="0.1.0",
+    version="0.1.1",
     description="Python Library of C++ based Basic MPC Template",
     author="Xiaonan (Nice) Wang <wangxiaonannice@gmail.com>",
     packages=["mpc_python"],
