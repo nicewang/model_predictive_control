1ac19e02485d54c3dad8fa7ab73c7a1356e30596
nicewang <wangxiaonannice@gmail.com>
2026-03-25 17:00:41 +0800

    cmt-20260325-2
    version 0.2.0
    
    Signed-off-by: nicewang <wangxiaonannice@gmail.com>

--- mpc_python/cpp/mpc.cpp
+++ mpc_python/cpp/mpc.cpp
@@ -79,6 +79,7 @@ void MPC::buildPredictionMatrices(Eigen::MatrixXd& Sx, Eigen::MatrixXd& Su) {
         Sx.block(k * A_.rows(), 0, A_.rows(), A_.rows()) = A_power;
         
         // Fill Su: coefficients of control inputs
+        // BUG
         // Eigen::MatrixXd A_power_temp = Eigen::MatrixXd::Identity(A_.rows(), A_.rows());
         // for (int j = 0; j <= k; ++j) {
         //     Su.block(k * A_.rows(), j * B_.cols(), A_.rows(), B_.cols()) = A_power_temp * B_;
@@ -86,9 +87,9 @@ void MPC::buildPredictionMatrices(Eigen::MatrixXd& Sx, Eigen::MatrixXd& Su) {
         //         A_power_temp = A_ * A_power_temp;
         //     }
         // }
-        // BUG-FIX
-        //    |
-        //    v
+        // FIX
+        //  |
+        //  v
         Eigen::MatrixXd A_power_temp = Eigen::MatrixXd::Identity(A_.rows(), A_.rows());
         for (int j = k; j >= 0; --j) {
             // Note: j decreases in reverse order to ensure that u(k) corresponds to B and u(0) corresponds to A^k * B.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mpc_python",
-    version="0.1.1",
+    version="0.2.0",
     description="Python Library of C++ based Basic MPC Template",
     author="Xiaonan (Nice) Wang <wangxiaonannice@gmail.com>",
     packages=["mpc_python"],
